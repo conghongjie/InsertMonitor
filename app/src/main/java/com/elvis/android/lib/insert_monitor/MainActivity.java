5642b0b144e24a240fbe406f86c37f218f05e2f2
conghongjie <conghongjie@bytedance.com>
2018-06-21 18:32:30 +0800

    readme

--- app/src/main/java/com/elvis/android/lib/insert_monitor/MainActivity.java
+++ app/src/main/java/com/elvis/android/lib/insert_monitor/MainActivity.java
@@ -3,7 +3,6 @@ package com.elvis.android.lib.insert_monitor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
 }
